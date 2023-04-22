75dbd513fe2437f778d6cfade9784247a495c020
jordy <jdesparza@utpl.edu.ec>
2023-04-22 18:05:51 -0500

    lisItem

---
+++ src/components/ListSongsHome/index.js
@@ -0,0 +1,159 @@
+import { View, Text, ContextCompat, Image, FlatList } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import styles from './styles'
+import MediaMeta from "rn-media-meta";
+
+import RNFS from 'react-native-fs';
+import { checkMultiple, PERMISSIONS, requestMultiple } from 'react-native-permissions';
+import repositorySongs from '../../data/repositorySongs';
+
+const ListSongsHome = () => {
+
+    const [isPermission, setIsPermission] = useState(false)
+    const [musicFiles, setMusicFiles] = useState([]);
+
+    const [isMusicList, setIsMusicList] = useState(0)
+    const [musicInfo, setMusicInfo] = useState([]);
+
+
+
+
+    // const requestMultiplePermisssion = () => {
+    //     if (Platform.OS === 'android') {
+    //         requestMultiple([PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE, PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE]).then((response) => {
+    //             // console.log(response)
+
+    //             if (
+    //                 response[PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE] === 'granted' &&
+    //                 response[PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE] === 'granted'
+    //             ) {
+    //                 console.log('granted')
+    //                 setIsPermission(true)
+    //             } else {
+    //                 console.log('denegado')
+    //                 setIsPermission(false)
+    //             }
+    //         })
+    //     }
+    // }
+
+    // const searchMusicFiles = async (folderPath, musicFiles) => {
+    //     const files = await RNFS.readDir(folderPath);
+    //     await Promise.all(
+    //         files.map(async file => {
+    //             if (file.isDirectory()) {
+    //                 if (file.name === 'Android' || file.name === 'Voice Recorder') { // Ignorar la carpeta "Android"
+    //                     return;
+    //                 }
+    //                 await searchMusicFiles(file.path, musicFiles);
+    //                 // console.log(file.path)
+    //             } else {
+    //                 const extension = file.name.split('.').pop();
+    //                 if (extension === 'mp3' || extension === 'wav' || extension === 'm4a') {
+    //                     musicFiles.push(file.path);
+    //                     await MediaMeta.get(file.path)
+    //                         .then(metadata => setMusicInfo(music => music.concat({ path: file.path, info: metadata })))
+    //                         .catch(err => console.error(err));
+    //                     // console.log('MUSICA =>=>=>=>=>=>=>=> : ', file.path)
+    //                     // console.log(musicInfo)
+    //                 }
+    //             }
+    //         })
+    //     );
+    //     return musicFiles;
+    // };
+
+    // const accesMemory = (pathExternal) => {
+    //     const paths = [RNFS.ExternalStorageDirectoryPath, pathExternal]
+
+    //     paths.forEach((element, index) => {
+    //         searchMusicFiles(element, [])
+    //             .then(musicFiles => {
+    //                 // index === 0 ? setMusicFilesInternal(musicFiles) : setMusicFilesExternal(musicFiles)
+    //                 setMusicFiles(currentMusic => currentMusic.concat(musicFiles))
+    //                 // console.log('\n\n\nMusic files in internal storage:', musicFiles);
+    //             });
+    //     })
+    // }
+
+    // const splitPath = (path) => {
+    //     let pathFinish = '/'
+    //     path.split('/').forEach((element, index) => {
+    //         if (element != '' && index <= 2) {
+    //             pathFinish += element + '/'
+    //         }
+    //     });
+
+    //     return (pathFinish)
+    // }
+
+    // const getSDcard = async () => {
+
+    //     const storageInternal = splitPath(RNFS.ExternalStorageDirectoryPath);
+    //     let storageExternal = []
+
+    //     if (Platform.OS === "android") {
+    //         const paths = (await RNFS.getAllExternalFilesDirs()).filter(
+    //             (path) => {
+    //                 storageExternal.push(splitPath(path))
+    //             }
+    //         );
+    //     }
+    //     storageExternal.forEach(element => {
+    //         if (element != storageInternal) {
+    //             accesMemory(element)
+    //         }
+    //     })
+    // }
+
+    // useEffect(() => {
+    //     if (!isPermission) requestMultiplePermisssion()
+    //     if (isPermission && musicFiles.length === 0) {
+    //         getSDcard()
+    //     }
+    // }, [isPermission])
+
+    // useEffect(() => {
+    //     console.log('Busqueda Musica: ', musicFiles.length)
+    // }, [musicFiles])
+
+
+    return (
+        <View style={styles.main}>
+            {musicInfo.length === 0 && (
+                <FlatList
+                    data={repositorySongs}
+                    // initialNumToRender={musicInfo.length}
+                    // keyExtractor={(item, index) => item.path}
+                    keyExtractor={(item, index) => item.id}
+                    renderItem={({ item }) => {
+                        if (item.info.thumb === '' || item.info.thumb === undefined || item.info.thumb === null) console.log(item)
+                        return (
+                            <View style={styles.itemContainer}>
+                                <Image
+                                    style={styles.itemImg}
+                                    source={
+                                        (item.info.thumb === '' || item.info.thumb === undefined || item.info.thumb === null) ?
+                                            require('../../assets/images/music.jpg')
+                                            :
+                                            { uri: `data:image/png;base64,${item.info.thumb}` }
+                                    }
+                                />
+                                <View style={styles.infoSongsCont}>
+                                    <Text style={styles.title} numberOfLines={1}>{item.info.title}</Text>
+                                    <Text style={styles.artist} numberOfLines={1}>{item.info.artist}</Text>
+                                </View>
+                            </View>
+                        )
+                    }}
+                    // numColumns={3}
+                    onEndReachedThreshold={0.1}
+                    showsVerticalScrollIndicator={false}
+                // maxToRenderPerBatch={15}
+                />
+            )}
+        </View>
+    )
+}
+
+export default ListSongsHome
--- src/components/ListSongsHome/styles.js
+++ src/components/ListSongsHome/styles.js
@@ -1,12 +1,52 @@
 import { StyleSheet, Dimensions, Platform, StatusBar } from 'react-native';
+import { COLOR_PRIMARY, COLOR_QUATERNARY, COLOR_SECONDARY, COLOR_TERTIARY, } from '../../utils/paleta';
 
 const { width, height } = Dimensions.get('window')
+
+const HEIGHT_ITEM = 55
 
 const styles = StyleSheet.create({
     main: {
         flex: 1,
         backgroundColor: 'rgba(250, 250, 250, 1)',
-        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+        // paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+    },
+    itemContainer: {
+        flex: 1,
+        height: HEIGHT_ITEM,
+        flexDirection: 'row',
+        backgroundColor: COLOR_QUATERNARY,
+        marginTop: 7,
+        overflow: 'hidden',
+        marginHorizontal: 10,
+        // borderRadius: 30, // 13
+        borderTopLeftRadius: HEIGHT_ITEM,
+        borderBottomLeftRadius: HEIGHT_ITEM
+    },
+    itemImg: {
+        width: HEIGHT_ITEM,
+        height: '100%',
+        borderTopRightRadius: HEIGHT_ITEM,
+        borderBottomRightRadius: HEIGHT_ITEM,
+        resizeMode: 'cover'
+    },
+    infoSongsCont: {
+        flex: 1,
+        height: '100%',
+        justifyContent: 'space-between',
+        paddingLeft: 5,
+        paddingRight: 15,
+        paddingVertical: 7
+    },
+    title: {
+        color: COLOR_PRIMARY,
+        fontSize: 15,
+        fontWeight: '500'
+    },
+    artist: {
+        color: COLOR_SECONDARY,
+        fontSize: 12,
+
     }
 })
 
